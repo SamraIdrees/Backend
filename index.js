464c785f054ab8d8e1a5af7c3712ebf11f76a021
Samra Idrees <samrabhatti678@gmail.com>
2025-05-02 19:26:42 +0500

    Updated feature X and fixed bug Y

--- index.js
+++ index.js
@@ -2,9 +2,6 @@ import dotenv from "dotenv";
 import dbConnection from "./databaseConnection/dbconnection.js";
 import { app } from "./app.js";
 
-import chatRoutes from "./routes/chatRoutes.js";
-app.use("/api/chat", chatRoutes);
-
 
 // Load environment variables from the .env file
 dotenv.config();
